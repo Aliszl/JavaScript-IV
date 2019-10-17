2fc0a8ae7e755ac8c47755062216e8ccaa51c60f
Alison Law <alisparklaw@gmail.com>
2019-10-17 16:05:14 +0100

    child/ instructor built

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -17,20 +17,52 @@
 
 class Person{
 
-constructor(name, age, location){
-    this.name = name;
-    this.age = age;
-    this.location = location;
+constructor(args){
+    this.name = args.name;
+    this.age = args.age;
+    this.location = args.location;
 }
 speak() {
     return `Hello my name is ${this.name}, I am from ${this.location}`;
 }
 } 
 
-const jonSnow = new Person ("Jon Snow", 25,"Castle Black") 
-;
+const jonSnow = new Person ({
+    name:"Jon Snow",
+    age: 25,
+    location:"Castle Black", 
+});
 
+
+class Instructor extends Person {
+    constructor (args){
+        super (args);
+        this.specialty = args.specialty;
+        this.favLanguage = args.favLanguage;
+        this.catchPhrase = args.catchPhrase;
+    }
+    // demo(subject){
+    //     return `Today we are learning about ${this.subject}`
+    // }
+    // grade(student, subject){
+    //     '${student.name} receives a perfect score on ${subject}'
+
+    // }
+}
+
+
+const aryaStark = new Instructor({
+    name: "Arya Stark",
+    age: 16,
+    location: "Winterfell",
+    specialty: "killing",
+    favLanguage: "javascript", 
+    catchPhrase:"A girl has no name",
+});
 
 // console.log(Person);
 console.log(jonSnow);
 console.log(jonSnow.speak());
+console.log(aryaStark);
+
+// console.log(aryaStark.demo("stick-fighting whilst blindfolded");)
